d35eb10557b9b1d83fe43b97f81d4a64b1e79acd
eugsong <eug.song@gmail.com>
2015-06-08 18:25:54 -0700

    Allows me to type in commands (i.e. G-code). Have tested the G-code commands and they work. However, right now it gets stuck waiting for the next response from Grbl when there is none. Need to fix that.

--- ArduinoUSB2/ArduinoUSB2.cpp
+++ ArduinoUSB2/ArduinoUSB2.cpp
@@ -41,63 +41,38 @@ int main(array<System::String ^> ^args)
 
 	String^ answer;
 	String^ portName;
-	String^ response1;
-	String^ response2;
-	String^ response3;
-	int baudRate = 500000;
+	String^ response;
+	int baudRate = 115200;
 	Console::WriteLine("Type in a port name and hit ENTER");
 	portName = Console::ReadLine();
 
 	SerialPort^ arduino;
 	arduino = gcnew SerialPort(portName, baudRate);
 	arduino->Open();
-	int i = 0;
-	do{
 
-		arduino->WriteLine("1");
-		arduino->WriteLine("1");
-		arduino->WriteLine("1");
-		StartCounter();
-		response1 = arduino->ReadLine();
-		response2 = arduino->ReadLine();
-		response3 = arduino->ReadLine();
-		Console::WriteLine(response1);
-		Console::WriteLine(response2);
-		Console::WriteLine(response3);
+	String^ input;
+	while (true)
+	{
+
+		input = Console::ReadLine();
+
+		if (input->Equals("exit")) break;
+
+		arduino->WriteLine(input);
+		response = arduino->ReadLine();
+		while (!String::IsNullOrEmpty(response))
+		{
+			Console::WriteLine(response);
+			response = arduino->ReadLine();
+		}
+
 		timefile << GetCounter() << "\n";
-		//serialfile << response1 << ", " << response2 << ", " << response3;
-		i++;
-		/*
-		// ask on or off
-		Console::WriteLine("Type \"on\" to turn the light on or \"off\" to turn it off");
-		// get answer
-		answer = Console::ReadLine();
-		//check that user typed one of the options
-		if (String::Compare(answer, "on") == 0)
-		{
-			arduino->WriteLine("1"); // send 1 to arduino
-			response = arduino->ReadLine();
-			Console::WriteLine(response);
-		}
-		else if (String::Compare(answer, "off") == 0)
-		{
-			arduino->WriteLine("0"); // send 0 to arduino
-			response = arduino->ReadLine();
-			Console::WriteLine(response);
-		}
-		else
-			Console::WriteLine(answer + " was not an option");
-		// ask user if he wants to continue
-		Console::WriteLine("Try again? yes/no");
-		// get answer
-		answer = Console::ReadLine();
-		// clear the screen
-		Console::Clear(); // clear the command line
-		*/
-	} while (i<1000); // while (String::Compare(answer, "yes") == 0);
+	}
+
 	timefile.close();
-	Console::ReadLine();
 	Console::Clear();
+	
+	arduino->Close();
 
 	return 0;
 }
